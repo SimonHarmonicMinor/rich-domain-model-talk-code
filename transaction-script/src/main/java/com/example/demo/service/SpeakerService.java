52c7bac9209a5fa486809c426569f46eee0162db
Семен Киреков <sakireko@mts.ru>
2023-11-08 11:38:53 +0300

    add SpeakerService

---
+++ transaction-script/src/main/java/com/example/demo/service/SpeakerService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class SpeakerService {
+
+}
