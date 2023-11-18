6650df14bd06c90401192f4d4993e71e60eef74e
Semyon Kirekov <semyon@kirekov.com>
2023-11-18 12:23:01 +0300

    fix speaker as rich entity

--- src/main/java/com/example/demo/domain/Speaker.java
+++ src/main/java/com/example/demo/domain/Speaker.java
@@ -1,5 +1,9 @@
-package com.example.demo.domain;
+    package com.example.demo.domain;
 
+import com.example.demo.domain.Talk.Status;
+import com.example.demo.exception.CannotAcceptTalkException;
+import com.example.demo.exception.CannotRejectTalkException;
+import com.example.demo.exception.CannotSubmitTalkException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,15 +12,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.Objects;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
-@Setter
-public class Speaker {
+@NoArgsConstructor(access = PROTECTED)
+public class Speaker extends AggregateRoot<Speaker> {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
@@ -30,8 +37,50 @@ public class Speaker {
     @OneToMany(mappedBy = "speaker")
     private List<Talk> talks = new ArrayList<>();
 
-    public void addTalk(Talk talk) {
-        talk.setSpeaker(this);
-        this.talks.add(talk);
+    public static Speaker newSpeaker(String firstName, String lastName) {
+        Speaker speaker = new Speaker();
+        speaker.firstName = firstName;
+        speaker.lastName = lastName;
+        return speaker;
+    }
+
+    public int submitTalk(String title) {
+        boolean experienced = countTalksByStatus(Status.ACCEPTED) >= 10;
+        int maxSubmittedTalksCount = experienced ? 3 : 5;
+        if (countTalksByStatus(Status.SUBMITTED) >= maxSubmittedTalksCount) {
+            throw new CannotSubmitTalkException("Submitted talks count is maximum: " + maxSubmittedTalksCount);
+        }
+        Talk talk = Talk.newTalk(this, Status.SUBMITTED, title, talks.size() + 1);
+        talks.add(talk);
+        return talk.getNumber();
+    }
+
+    private long countTalksByStatus(Talk.Status status) {
+        return talks.stream().filter(t -> t.getStatus().equals(status)).count();
+    }
+
+    public void acceptTalk(int talkNumber) {
+        Talk talk = talkByNumber(talkNumber);
+        talk.setStatus(status -> {
+            if (!status.equals(Status.SUBMITTED)) {
+                throw new CannotAcceptTalkException("");
+            }
+            return Status.ACCEPTED;
+        });
+    }
+
+    public void rejectTalk(int talkNumber) {
+        Talk talk = talkByNumber(talkNumber);
+        talk.setStatus(status -> {
+            if (!status.equals(Status.SUBMITTED)) {
+                throw new CannotRejectTalkException("");
+            }
+            return Status.REJECTED;
+        });
+        registerEvent(new TalkRejectedEvent(this.id, talkNumber));
+    }
+
+    private Talk talkByNumber(int number) {
+        return talks.stream().filter(t -> Objects.equals(t.getNumber(), number)).findFirst().orElseThrow();
     }
 }
