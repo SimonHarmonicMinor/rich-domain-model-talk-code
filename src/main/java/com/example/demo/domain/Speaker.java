bd6f19122ddd319cc1c2ac136d92a14c35fbdfa9
Semyon Kirekov <semyon@kirekov.com>
2023-11-25 13:24:09 +0300

    remove imports

--- src/main/java/com/example/demo/domain/Speaker.java
+++ src/main/java/com/example/demo/domain/Speaker.java
@@ -1,86 +1,87 @@
-    package com.example.demo.domain;
+package com.example.demo.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 import com.example.demo.domain.Talk.Status;
 import com.example.demo.exception.CannotAcceptTalkException;
 import com.example.demo.exception.CannotRejectTalkException;
 import com.example.demo.exception.CannotSubmitTalkException;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.AbstractAggregateRoot;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class Speaker extends AggregateRoot<Speaker> {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
 
-    @Column(name = "first_name")
-    private String firstName;
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
 
-    @Column(name = "last_name")
-    private String lastName;
+  @Column(name = "first_name")
+  private String firstName;
 
-    @OneToMany(mappedBy = "speaker")
-    private List<Talk> talks = new ArrayList<>();
+  @Column(name = "last_name")
+  private String lastName;
 
-    public static Speaker newSpeaker(String firstName, String lastName) {
-        Speaker speaker = new Speaker();
-        speaker.firstName = firstName;
-        speaker.lastName = lastName;
-        return speaker;
+  @OneToMany(mappedBy = "speaker")
+  private List<Talk> talks = new ArrayList<>();
+
+  public static Speaker newSpeaker(String firstName, String lastName) {
+    Speaker speaker = new Speaker();
+    speaker.firstName = firstName;
+    speaker.lastName = lastName;
+    return speaker;
+  }
+
+  public int submitTalk(String title) {
+    boolean experienced = countTalksByStatus(Status.ACCEPTED) >= 10;
+    int maxSubmittedTalksCount = experienced ? 3 : 5;
+    if (countTalksByStatus(Status.SUBMITTED) >= maxSubmittedTalksCount) {
+      throw new CannotSubmitTalkException(
+          "Submitted talks count is maximum: " + maxSubmittedTalksCount);
     }
+    Talk talk = Talk.newTalk(this, Status.SUBMITTED, title, talks.size() + 1);
+    talks.add(talk);
+    return talk.getNumber();
+  }
 
-    public int submitTalk(String title) {
-        boolean experienced = countTalksByStatus(Status.ACCEPTED) >= 10;
-        int maxSubmittedTalksCount = experienced ? 3 : 5;
-        if (countTalksByStatus(Status.SUBMITTED) >= maxSubmittedTalksCount) {
-            throw new CannotSubmitTalkException("Submitted talks count is maximum: " + maxSubmittedTalksCount);
-        }
-        Talk talk = Talk.newTalk(this, Status.SUBMITTED, title, talks.size() + 1);
-        talks.add(talk);
-        return talk.getNumber();
-    }
+  private long countTalksByStatus(Talk.Status status) {
+    return talks.stream().filter(t -> t.getStatus().equals(status)).count();
+  }
 
-    private long countTalksByStatus(Talk.Status status) {
-        return talks.stream().filter(t -> t.getStatus().equals(status)).count();
-    }
+  public void acceptTalk(int talkNumber) {
+    Talk talk = talkByNumber(talkNumber);
+    talk.setStatus(status -> {
+      if (!status.equals(Status.SUBMITTED)) {
+        throw new CannotAcceptTalkException("");
+      }
+      return Status.ACCEPTED;
+    });
+  }
 
-    public void acceptTalk(int talkNumber) {
-        Talk talk = talkByNumber(talkNumber);
-        talk.setStatus(status -> {
-            if (!status.equals(Status.SUBMITTED)) {
-                throw new CannotAcceptTalkException("");
-            }
-            return Status.ACCEPTED;
-        });
-    }
+  public void rejectTalk(int talkNumber) {
+    Talk talk = talkByNumber(talkNumber);
+    talk.setStatus(status -> {
+      if (!status.equals(Status.SUBMITTED)) {
+        throw new CannotRejectTalkException("");
+      }
+      return Status.REJECTED;
+    });
+    registerEvent(new TalkRejectedEvent(this.id, talkNumber));
+  }
 
-    public void rejectTalk(int talkNumber) {
-        Talk talk = talkByNumber(talkNumber);
-        talk.setStatus(status -> {
-            if (!status.equals(Status.SUBMITTED)) {
-                throw new CannotRejectTalkException("");
-            }
-            return Status.REJECTED;
-        });
-        registerEvent(new TalkRejectedEvent(this.id, talkNumber));
-    }
-
-    private Talk talkByNumber(int number) {
-        return talks.stream().filter(t -> Objects.equals(t.getNumber(), number)).findFirst().orElseThrow();
-    }
+  private Talk talkByNumber(int number) {
+    return talks.stream().filter(t -> Objects.equals(t.getNumber(), number)).findFirst()
+        .orElseThrow();
+  }
 }
